5204539197eab4038cd39798710d49973a6575a7
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2024-07-31 00:32:04 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDB = async () => {
    try {
-      await mongoose.connect("mongodb+srv://shrinkhalashringar000:bKzIUeZxnOTc2UXU@cluster0.7gmrqyw.mongodb.net/Bloodbank");
+      await mongoose.connect("process.env.MONGO_URL");
       console.log(`Connected to Mongodb Database ${mongoose.connection.host}`.bgCyan.white);
    } catch (error) {
       console.log(`Mongodb Database Error ${error}`.bgRed.white);
