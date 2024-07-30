b81abf70d2de79bf42157e11b370635efa17da7a
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2024-07-31 00:33:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDB = async () => {
    try {
-      await mongoose.connect("process.env.MONGO_URL");
+      await mongoose.connect(process.env.MONGO_URL);
       console.log(`Connected to Mongodb Database ${mongoose.connection.host}`.bgCyan.white);
    } catch (error) {
       console.log(`Mongodb Database Error ${error}`.bgRed.white);
