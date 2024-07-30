da586e012ebbfbaff433e6a6e3d616d4f0071a76
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2024-07-30 19:31:01 +0530

    Update Consumer.js

--- client/src/pages/Dashboard/Consumer.js
+++ client/src/pages/Dashboard/Consumer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Layout from '../../components/shared/Layout/Layout';
 import moment from 'moment';
 import API from '../../services/API';
@@ -7,7 +7,7 @@ const Consumer = () => {
     const { user } = useSelector((state) => state.auth);
     const [data, setData] = useState([]);
     //find donar records
-    const getDonars = async () => {
+    const getDonars = useCallback(async () => {
         try {
             const { data } = await API.post('/inventory/get-inventory-hospital', {
                 filters: {
@@ -21,10 +21,10 @@ const Consumer = () => {
         } catch (error) {
             console.log(error);
         }
-    };
+    }, [user]);
     useEffect(() => {
         getDonars();
-    }, []);
+    }, [getDonars]);
     return (
         <Layout>
             <div className='container mt-4'>
