9e766e993be1eaea9cd1726f70a992af05ee41f1
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2024-07-30 20:28:57 +0530

    Update API.js

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: 'https://blood-bank-app-nw2y.onrender.com' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("token")) {
         req.headers.Authorization = `Bearer ${localStorage.getItem("token")} `;
