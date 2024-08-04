0f3cab4fe7d2fd71fda4c5b820ae56428c101bf7
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2024-08-04 19:57:51 +0530

    Update Login.js

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
             ) : (
                 <div className='row g-0'>
                     <div className='col-md-8 form-banner'>
-                        <img src="./assets/images/Login page.jpg" className='d-flex d-wrap' alt="loginImage" />
+                        <img src="./assets/images/Login_Page_Image.jpg" className='d-flex d-wrap' alt="loginImage" />
                     </div>
                     <div className='col-md-4 form-container'>
                         <Form formTitle={"Login Page"} submitBtn={"Login"} formType={"login"} />
